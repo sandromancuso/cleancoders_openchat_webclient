ef3f4978863e83e4d103d1b0937ba04c7ebc40bb
Nacho García <lupus.zgz@gmail.com>
2018-01-05 10:39:42 +0100

    Add logout button

--- src/__tests__/components/Header.test.js
+++ src/__tests__/components/Header.test.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Header from 'components/Header'
+import userService from 'services/User'
+import User from 'domain/User'
+
+const aUser = new User({ id: 'some id', name: 'some name', about: 'about him' })
+const router = createMockRouter()
+const context = { router }
 
 describe('Header', () => {
-  it('renders', () => {
-    const wrapper = shallow(<Header />)
+  let wrapper
 
-    expect(wrapper.html()).toMatchSnapshot()
+  beforeEach(() => {
+    userService.user = aUser
+    wrapper = shallow(<Header />, { context })
   })
+
+  it('has the logout when the user is logged', () => {
+    expect(wrapper.find('.logout')).toHaveLength(1)
+  })
+
+  it('does not have the logout when the user is not logged', () => {
+    userService.user = null
+    wrapper = shallow(<Header />, { context })
+
+    expect(wrapper.find('.logout')).toHaveLength(0)
+  })
+
+  it('logs out', () => {
+    expect(wrapper.find('.logout')).toHaveLength(1)
+  })
+
 })
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+import userService from 'services/User'
 
 class Header extends Component {
+  static contextTypes = {
+    router: PropTypes.object.isRequired
+  }
+
+  logout() {
+    userService.logout()
+    this.context.router.history.push('/')
+  }
+
   render() {
+      const button = userService.user?
+        (<button className="logout btn btn-outline-light" onClick={this.logout.bind(this)}>
+          <i className="fa fa-sign-out" /> Log Out
+        </button>) :
+      null
+
     return (
-      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a className="navbar-brand" href="/">OpenChat</a>
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary fixed-top justify-content-between">
+        <Link to="/" className="navbar-brand">OpenChat</Link>
+        {button}
       </nav>
     )
   }
