609a689b3758ab470e7d94aa6ce1ba2e908b17f9
Nacho García <lupus.zgz@gmail.com>
2018-01-05 09:40:53 +0100

    Add snapshot serializer

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,10 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Serializer from 'jest-serializer-html'
 
 configure({ adapter: new Adapter() });
+
+expect.addSnapshotSerializer(Serializer)
 
 global.flushPromises = () => new Promise(resolve => setImmediate(resolve))
 
