a9ff2039135ff11a658a802939071fb2e422c024
Nacho García <lupus.zgz@gmail.com>
2018-01-06 18:52:44 +0100

    Remove semicolons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import userService from 'services/User'
 
