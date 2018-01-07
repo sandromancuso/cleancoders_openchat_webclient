c60b90e1022d0b6723a53c465d5799bf2b99d3e0
Nacho García <lupus.zgz@gmail.com>
2018-01-07 13:40:42 +0100

    Display own username on posts diferently

--- src/__tests__/components/Post.test.js
+++ src/__tests__/components/Post.test.js
@@ -2,20 +2,50 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { Link } from 'react-router-dom'
 import Post from 'components/Post'
-import { aPost, aUser } from 'testFixtures'
+import userService from 'services/User'
+import { aPost, aUser, anotherUser } from 'testFixtures'
 
 const router = createMockRouter()
 const context = { router }
 
 describe('Post', () => {
-  it('displays link and data', () => {
-    const wrapper = shallow(<Post post={aPost} user={aUser} />, { context })
-    const link = wrapper.find(Link)
+  let wrapper
 
-    expect(link).toHaveLength(1)
-    expect(link.prop('to')).toEqual(`/wall/${aUser.id}`)
-    expect(wrapper.text().includes(aPost.text)).toBe(true)
-    expect(wrapper.text().includes(aPost.date)).toBe(true)
-    expect(wrapper.text().includes(aPost.time)).toBe(true)
+  describe('of another poster', () => {
+    beforeEach( () => {
+      userService.user = aUser
+      wrapper = shallow(<Post post={aPost} user={anotherUser} />, { context })
+    })
+
+    it('shows wall link', () => {
+      const link = wrapper.find(Link)
+
+      expect(link.prop('to')).toEqual(`/wall/${anotherUser.id}`)
+    })
+
+    it('shows post data', () => {
+      expect(wrapper.text().includes(aPost.text)).toBe(true)
+      expect(wrapper.text().includes(aPost.date)).toBe(true)
+      expect(wrapper.text().includes(aPost.time)).toBe(true)
+    })
+  })
+
+  describe('of the own poster', () => {
+    beforeEach( () => {
+      userService.user = aUser
+      wrapper = shallow(<Post post={aPost} user={aUser} />, { context })
+    })
+
+    it('shows wall link', () => {
+      const link = wrapper.find(Link)
+
+      expect(link.prop('to')).toEqual(`/wall`)
+    })
+
+    it('shows post data', () => {
+      expect(wrapper.text().includes(aPost.text)).toBe(true)
+      expect(wrapper.text().includes(aPost.date)).toBe(true)
+      expect(wrapper.text().includes(aPost.time)).toBe(true)
+    })
   })
 })
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import userService from 'services/User'
 
 const Post = ({ post, user }) => {
+
+  const isOwnPost = user.id === userService.user.id
 
   return (
     <div className="card my-3">
       <div className="card-header">
         <div className="row justify-content-between">
           <div className="col-6">
-            <Link to={`/wall/${user.id}`}>{user.name}</Link>
+            {isOwnPost ?
+              <Link to='/wall'>You</Link> :
+              <Link to={`/wall/${user.id}`}>{user.name}</Link>
+            }
           </div>
           <div className="col-6 text-right text-muted">
             Posted on {post.date} at {post.time}
