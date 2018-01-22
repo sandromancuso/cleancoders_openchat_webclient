b5a0a752550cf08293e7340eebfbac8e47aec285
Nacho García <lupus.zgz@gmail.com>
2018-01-22 23:13:27 +0100

    Remove collapsing navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,35 +16,21 @@ class Header extends Component {
   render() {
     const button = (
       <button
-        className="logout btn btn-outline-light"
+        className="nav-item logout btn btn-outline-light"
         onClick={this.logout.bind(this)}
       >
         <i className="fa fa-sign-out" /> Log Out
       </button>)
 
-    const usersToFollow = <Link className="nav-link mr-3" to="/findUsers">Find users to follow</Link>
+    const usersToFollow = <Link className="nav-item nav-link mr-3" to="/findUsers">Follow users</Link>
 
     return (
-      <nav className="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
+      <nav className="navbar navbar-expand navbar-dark bg-primary fixed-top">
         <Link to="/" className="navbar-brand">OpenChat</Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbar"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
-        <div className="navbar-collapse collapse justify-content-stretch" id="navbar">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              {userService.user? usersToFollow : null}
-            </li>
-            <li className="nav-item">
-              {userService.user? button : null}
-            </li>
-          </ul>
+        <div className="navbar-nav ml-auto">
+          {userService.user? usersToFollow : null}
+          {userService.user? button : null}
         </div>
       </nav>
     )
