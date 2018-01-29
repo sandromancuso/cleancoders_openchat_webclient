ab336f1293e43948511f0799165cfd6ac8013b7d
Nacho García <lupus.zgz@gmail.com>
2018-01-29 23:36:09 +0100

    Use scss with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import 'App.css'
 import { BrowserRouter } from 'react-router-dom'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
