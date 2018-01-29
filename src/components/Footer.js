bc2055b13342de89b82c22b1ee17355b74031042
Nacho García <lupus.zgz@gmail.com>
2018-01-29 23:35:32 +0100

    Add company url to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render() {
     return (
-      <footer className="footer">
-        <div className="container text-muted text-right">
-          Crafted with <i className="fa fa-heart text-danger"></i>
+      <footer className="footer text-muted">
+        <div className="container">
+          <div className="row">
+            <div className="col mr-auto">
+              <a href="https://codurance.com/" target="_blank" rel="noopener noreferrer">
+                Codurance
+              </a>
+            </div>
+            <div className="col text-right">
+              Crafted with <i className="fa fa-heart text-danger"></i>
+            </div>
         </div>
+      </div>
       </footer>
     )
   }
