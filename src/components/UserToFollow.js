2b10e28701ff77b0c5356671375c9540c9c6aefd
Nacho García <lupus.zgz@gmail.com>
2018-02-09 19:28:29 +0100

    Remove margin

--- src/components/UserToFollow.js
+++ src/components/UserToFollow.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default (props) => (
   <div className='card col-lg-3 col-md-5 col-sm-12 col-xs-12 user-to-follow m-1'>
     <div className='card-body'>
-      <p className='card-title mb-1'><i className='fa fa-user text-primary mr-1' /><Link to={`/wall/${props.user.id}`}><strong>{props.user.name}</strong></Link></p>
+      <p className='card-title'><i className='fa fa-user text-primary mr-1' /><Link to={`/wall/${props.user.id}`}><strong>{props.user.name}</strong></Link></p>
       <p className='card-text'>{props.user.about}</p>
       {!props.isFollowee
         ? (<button
