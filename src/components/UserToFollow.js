23ea4224ea8d9befcbc5a32ad509116feaa2fe84
Nacho García <lupus.zgz@gmail.com>
2018-02-09 19:45:24 +0100

    Remove unused class

--- src/components/UserToFollow.js
+++ src/components/UserToFollow.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default (props) => (
-  <div className='card col-lg-3 col-md-5 col-sm-12 col-xs-12 user-to-follow m-1'>
+  <div className='card col-lg-3 col-md-5 col-sm-12 col-xs-12 m-1'>
     <div className='card-body'>
-      <p className='card-title'><i className='fa fa-user text-primary mr-1' /><Link to={`/wall/${props.user.id}`}><strong>{props.user.name}</strong></Link></p>
+      <p className='card-title'>
+        <i className='fa fa-user text-primary mr-1' />
+        <Link to={`/wall/${props.user.id}`}>
+          <strong>{props.user.name}</strong>
+        </Link>
+      </p>
       <p className='card-text'>{props.user.about}</p>
       {!props.isFollowee
         ? (<button
