9112258cfb96fcb896f1d034a5c8f303da32893f
Nacho García <lupus.zgz@gmail.com>
2018-02-09 17:36:50 +0100

    Outline already following

--- src/components/UserToFollow.js
+++ src/components/UserToFollow.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default (props) => (
   <div className='card col-lg-3 col-md-5 col-sm-12 col-xs-12 user-to-follow m-1'>
     <div className='card-body'>
-      <p className='card-title mb-1'><i className='fa fa-user text-primary mr-1' /><Link to={`/wall/${props.user.id}`}>{props.user.name}</Link></p>
-      <p className='card-text small'>{props.user.about}</p>
+      <p className='card-title mb-1'><i className='fa fa-user text-primary mr-1' /><Link to={`/wall/${props.user.id}`}><strong>{props.user.name}</strong></Link></p>
+      <p className='card-text'>{props.user.about}</p>
       {!props.isFollowee
         ? (<button
           className='card-link btn btn-sm btn-primary'
@@ -13,7 +13,7 @@ export default (props) => (
           >
           <i className='fa fa-plus' /> Follow</button>
         )
-        : (<span>Already following</span>)
+        : (<span className='border rounded p-2 small'>Already following</span>)
       }
     </div>
   </div>
